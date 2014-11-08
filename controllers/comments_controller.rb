ff956ed46de33b0e15dc4ce76eea56e105f1ad48
bigtone1284 <anthony.defreitas@gmail.com>
2014-11-08 17:47:19 -0500

    created comments on a separate page.

--- controllers/comments_controller.rb
+++ controllers/comments_controller.rb
@@ -1,9 +1,15 @@
 post '/comments' do
-	binding.pry
 	comment = Comment.create({
 		document_id: params[:document_id],
 		commentary: params[:commentary],
 		author_id: params[:author_id]
 		})
-	redirect "/documents/#{comment.document_id}"
+	redirect "/comments/#{comment.document_id}"
+end
+
+get '/comments/:id' do 
+	@document = Document.find(params[:id])
+	@comments = @document.comments
+	@authors = Author.all
+	erb :'comments/show'
 end
